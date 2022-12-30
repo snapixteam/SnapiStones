095bbf1c6766f23022e4e703e60ebd9b9bfae21b
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2022-12-30 21:01:53 +0300

    Update IModule.java

--- src/main/java/ru/mcsnapix/snapistones/interfaces/IModule.java
+++ src/main/java/ru/mcsnapix/snapistones/interfaces/IModule.java
@@ -3,8 +3,5 @@ package ru.mcsnapix.snapistones.interfaces;
 import ru.mcsnapix.snapistones.SnapiStones;
 
 public interface IModule {
-
     void loadModule(SnapiStones plugin);
-
-//    void disable();
 }
